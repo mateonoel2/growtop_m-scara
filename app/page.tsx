a6b4a92f587c7d76ee5777a7368a1331cc22a331
mateonoel2 <mateo.noel@utec.edu.pe>
2024-05-16 15:25:09 -0500

    casi listo

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,13 @@ export default function Home() {
     const { data: session } = useSession();
     const [user, setUser] = useState<any>(null);
     const [empresa, setEmpresa] = useState<any>(null);
+    const [tasks, setTasks] = useState([
+        { id: 1, text: 'Prueba 1', completed: false },
+        { id: 2, text: 'Prueba 2', completed: false },
+        { id: 3, text: 'Prueba 3', completed: false },
+        { id: 4, text: 'Sesión en vivo', completed: false },
+    ]);
+
 
     const fetchUserData = async () => {
         if (session && session.user && 'id' in session.user) {
@@ -35,6 +42,14 @@ export default function Home() {
             fetchEmpresaData();
         }
     }, [user]);
+
+    const handleCheckboxChange = (id: number) => {
+        setTasks(prevTasks =>
+            prevTasks.map(task =>
+                task.id === id ? { ...task, completed: !task.completed } : task
+            )
+        );
+    };
 
     return (
         <>
@@ -83,7 +98,7 @@ export default function Home() {
                             </div>
 
                             <div className="my-4"></div>
-                            <div className="bg-white p-3 hover:shadow">
+                            <div className="bg-white p-3 hover:shadow rounded-sm">
                                 <div className="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8">
                                     <span className="text-green-500">
                                         <svg className="h-5 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -99,35 +114,6 @@ export default function Home() {
                                         <img src={empresa.logo} alt="logo" className="w-10 h-10 rounded-full border-2 border-gray-300" />
                                     </div>
                                     <div className="text-gray-700 mt-2">{empresa.name}</div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="w-full md:w-9/12 mx-2 h-64">
-
-                            <div className="bg-white p-3 shadow-sm rounded-sm">
-                                <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
-                                    <span className="text-green-500">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-                                    </span>
-                                    <span className="tracking-wide">Tareas Pendientes</span>
-                                </div>
-                                <div className="text-gray-700">
-                                    <div className="text-gray-700">
-                                        {user.links.map((link: string, index: number) => {
-                                            if (user.links_status[index] === "pending") {
-                                                return (
-                                                    <div key={index} className="grid grid-cols-2">
-                                                        <div className="text-gray-700 mt-3">
-                                                            <a href={link} target="_blank" rel="noreferrer">{link}</a>
-                                                        </div>
-                                                    </div>
-                                                );
-                                            }
-                                            return null;
-                                        })}
-                                    </div>
-
                                 </div>
                             </div>
 
@@ -147,18 +133,86 @@ export default function Home() {
                                     <span className="tracking-wide">Tareas Completadas</span>
                                 </div>
                                 <div className="text-gray-700">
-                                    {user.links.map((link: string, index: number) => {
-                                        if (user.links_status[index] === "approved") {
-                                            return (
-                                                <div key={index} className="grid grid-cols-2">
-                                                    <div className="text-gray-700 mt-3">
-                                                        <a href={link} target="_blank" rel="noreferrer">{link}</a>
-                                                    </div>
-                                                </div>
-                                            );
+                                    <div className="pt-4 pl-2">
+                                        <ul>
+                                            {tasks.map(task => (
+                                                <li key={task.id} className="flex items-center mb-2">
+                                                    <input
+                                                        type="checkbox"
+                                                        checked={task.completed}
+                                                        onChange={() => handleCheckboxChange(task.id)}
+                                                        className="mr-2"
+                                                    />
+                                                    <span className={task.completed ? 'line-through' : ''}>
+                                                        {task.text}
+                                                    </span>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-full md:w-9/12 mx-2 h-64">
+
+                            <div className="bg-white p-3 shadow-sm rounded-sm">
+                                <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
+                                    <span className="text-green-500">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                                    </span>
+                                    <span className="tracking-wide">Tareas Pendientes</span>
+                                </div>
+                                <div className="text-gray-700">
+                                    <div className="text-gray-700">
+
+                                        {user.links_status[0] === "pending" && (
+                                            <div className="mt-5 mb-5" >
+                                                <h4 className="text-gray-700 font-semibold text-lg">Prueba 1: Estilos de aprendizaje</h4>
+                                                <p className="mb-2"><span className="font-bold">Instrucciones:</span> Ingresa al link del test y complétalo con todos los datos que se requieren, no hay tiempo límite, recuerda que no hay respuesta buena o mala.</p>
+                                                <p className="mb-2">
+                                                    <span className="font-bold">Link:</span>  <a href={user.links[0]} target="_blank" rel="noreferrer" className="text-blue-500 hover:underline">{user.links[0]}</a>
+                                                </p>
+                                                <p className="mb-2"> <span className="font-bold">Nombre de usuario:</span>Regístrate con tus nombres, apellidos y correo en la plataforma.</p>
+                                                <p className="mb-2"><span className="font-bold">Código:</span> {empresa.codigo}</p>
+                                            </div>)
                                         }
-                                        return null;
-                                    })}
+
+                                        {user.links_status[1] === "pending" && (
+                                            <div className="mt-5 mb-5" >
+                                                <h4 className="text-gray-700 font-semibold text-lg">Prueba 2: Assessment de competencia</h4>
+                                                <p className="mb-2"><span className="font-bold">Instrucciones:</span> Te llegará un correo de bienvenida directamente de la plataforma LEIRO ASSESSMENTS con un link, tu correo y una contraseña para ingresar directamente a una plataforma con una imagen como la de arriba.</p>
+                                                <p className="mb-2">
+                                                    <span className="font-bold">Link:</span>  <a href={user.links[1]} target="_blank" rel="noreferrer" className="text-blue-500 hover:underline">{user.links[1]}</a>
+                                                </p>
+                                                <p className="mb-2"> <span className="font-bold">Nombre de usuario: </span>CORREO ELECTRÓNICO al que te llegó esta comunicación y la contraseña la podrás encontrar en el correo de “PRUEBA APL”</p>
+                                            </div>)
+                                        }
+
+                                        {user.links_status[2] === "pending" && (
+                                            <div className="mt-5 mb-5" >
+                                                <h4 className="text-gray-700 font-semibold text-lg">Prueba 3: Test de competencias transversales</h4>
+                                                <p className="mb-2"><span className="font-bold">Instrucciones:</span> Ingresa al link a continuación para acceder al sitio. La primera vez que inicie sesión en el sitio, deberá registrarse con nombres, apellidos y correo. Recuerda no cerrar el explorador cuando estés realizando la evaluación para evitar el cierre del sistema. </p>
+                                                <p className="mb-2">
+                                                    <span className="font-bold">Link:</span>  <a href={user.links[2]} target="_blank" rel="noreferrer" className="text-blue-500 hover:underline">{user.links[2]}</a>
+                                                </p>
+                                                <p className="mb-2"> <span className="font-bold">Nombre de usuario: </span>Regístrate con tus nombres, apellidos y correo en la plataforma.</p>
+                                            </div>)
+                                        }
+
+                                        {user.links_status[3] === "pending" && (
+                                            <div className="mt-5 mb-5" >
+                                                <h4 className="text-gray-700 font-semibold text-lg">Sesión en vivo: Dinámica de Assesment individual</h4>
+                                                <p className="mb-2"><span className="font-bold">Instrucciones:</span> Ingresa al link de CALENDLY y agenda tu sesión con un especialista de Growtop de acuerdo con los horarios disponibles para pasar esta evaluación a través de Microsoft Teams. Recuerda que la sesión individual dura una 1 hora y 30 minutos. Las indicaciones del reto que debes cumplir las conocerás durante la sesión, recuerda estar puntual en la sesión.</p>
+                                                <p className="mb-2">
+                                                    <span className="font-bold">Link:</span>  <a href={user.links[2]} target="_blank" rel="noreferrer" className="text-blue-500 hover:underline">{user.links[2]}</a>
+                                                </p>
+                                                <p className="mb-2"> <span className="font-bold">Nombre de usuario: </span>Regístrate con tus nombres, apellidos y correo en la plataforma.</p>
+                                            </div>)
+                                        }
+
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
