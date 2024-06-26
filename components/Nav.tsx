bb13ab3e36da5edc3f8d3b7a7a4c296e4bc18dbc
mateonoel2 <mateo.noel@utec.edu.pe>
2024-06-26 14:41:19 -0500

    diseño

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import NextLogo from "@/assets/images/next-logo";
+import GrowtopLogo from "@/assets/images/growtop-logo.svg";
+import Image from "next/image";
 import { signOut, useSession} from "next-auth/react";
 
 const Nav = () => {
@@ -10,10 +11,8 @@ const Nav = () => {
   return (
     <nav className='flex-between w-full mb-5 py-3'>
       <Link href='/' className='flex gap-2 flex-center max-w-100'>
-         <div className="h-7 w-40"> 
-            <NextLogo />
-          </div>
-          <p className='logo_text hover:text-green-400'>Assessment de Competencias</p>
+        <Image alt='logo' src={GrowtopLogo} className="h-7 w-40" /> 
+        <p className='logo_text hover:text-green-400'>Assessment de Competencias</p>
       </Link>
       
 
