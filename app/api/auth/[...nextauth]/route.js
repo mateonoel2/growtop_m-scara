5483938df18f2391605ec2bc2dcd639e10f738e1
Eduardo <ebautistaarrilucea@gamil.com>
2024-11-12 23:57:55 -0500

    .

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,27 @@
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import { SQLiteAdapter } from "../../../../utils/sqliteAdapter";
+import { ServerClient } from 'postmark'; // Importa ServerClient de postmark directamente
+
+// Configurar el cliente de Postmark
+const client = new ServerClient("POSTMARK_API_TEST"); // Utiliza directamente ServerClient
+
+// Configurar la función de envío de correos con Postmark
+async function sendVerificationRequest({ identifier, url, provider }) {
+  const { from } = provider;
+  await client.sendEmail({
+    From: from,
+    To: identifier,
+    Subject: "Inicia sesión en tu cuenta",
+    TextBody: `Inicia sesión en tu cuenta de Growtop usando el siguiente enlace:\n\n${url}\n\nEste enlace es válido solo por unos minutos.`,
+  });
+}
 
 const authOptions = {
   providers: [
     EmailProvider({
-      server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
+      sendVerificationRequest,
     }),
   ],
   adapter: SQLiteAdapter(),
