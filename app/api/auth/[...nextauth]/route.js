1cc1253601e4c21344f054976b5a7b7ca5b9b2e2
Eduardo <ebautistaarrilucea@gamil.com>
2024-11-28 00:08:38 -0500

    .

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,43 +1,43 @@
-import NextAuth from 'next-auth';
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
-import clientPromise from "@/utils/mongodb"
-import EmailProvider from "next-auth/providers/email"
-import { MyVerificationRequest } from '@/myVerificationRequest';
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/utils/mongodb";
+import EmailProvider from "next-auth/providers/email";
 
- const handler = NextAuth({
-    adapter: MongoDBAdapter(clientPromise),
-    providers: [
-        EmailProvider({
-            id: 'email',
-            name: 'email',
-            server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM,
-            sendVerificationRequest({identifier, url, provider,
-            }) {
-              MyVerificationRequest({identifier, url, provider})
-            },
-          }),
-    ],
-    callbacks: {
-      async session({ session, user }) {
-        session.user.id = user.id;
-        session.user.email = user.email;
-  
-        return session;
+// Configuración de NextAuth
+const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER,
+        port: 587,
+        auth: {
+          user: process.env.EMAIL_FROM,
+          pass: process.env.POSTMARK_API_KEY,
+        },
       },
-      async signIn({ user }) {
-        const db = await clientPromise;
-        const collection = db.db("test").collection('users');
-        
-        const allowedEmail = await collection.findOne({ email: user.email });
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+    async signIn({ user }) {
+      const db = await clientPromise;
+      const collection = db.db("growtopdb").collection("assessments"); // Cambiado de "users" a "assessments"
   
-        if (!allowedEmail) {
-          return Promise.reject(new Error('No cuentas con cuenta de growtop.'));
-        }
-  
-        return Promise.resolve(true);
-      },
-    }
-  })
-  
-  export { handler as GET, handler as POST }
+      const allowedEmail = await collection.findOne({ correo: user.email }); // Cambiado "email" a "correo"
+      if (!allowedEmail) {
+        throw new Error("No cuentas con cuenta de Growtop.");
+      }
+      return true;
+    },
+  },  
+  debug: true, // Habilita logs para depuración
+};
+
+// Exporta métodos específicos
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions);
